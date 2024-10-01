44cd2772641527960d727d3fcc9bf4174efd4148
Manubetim <126467453+Manubetim@users.noreply.github.com>
2024-10-01 09:19:09 -0300

    ddd

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,99 @@
-
-function criaCartao(categoria, pergunta, resposta){
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-
-    cartao.innerHTML = `
-    <div class="cartao_conteudo">
-        <h3>${categoria}</h3>
-        <div class="cartao_conteudo_pergunta">
-            <p>${pergunta}</p>
-        </div>
-        <div class="cartao_conteudo_resposta"
-        <p>${resposta}</p>
-    </div>
-    </div>
-    `
-    container.appendChild(cartao);
-}
+criaCartao (
+    'Matematica' ,
+    'Como se calcula o seno de um ângulo?',
+     'sen=ca/hi')
+    
+    criaCartao (
+    'História',
+    'Quando aconteceu a revolução francesa?',
+    '1789')
+    
+    criaCartao (
+    'História',
+    'Quando aconteceu a primeira guerra mundial?',
+    '1914')
+    
+    criaCartao (
+    'Geograia',
+    'Qual é ao maior continente do mundo?',
+    'Ásia')
+    
+    criaCartao (
+    'Geografia',
+    'Qua é o maior país em estado territorial?',
+    'Rússia')
+    
+    criaCartao (
+    'Quimica',
+    'O que é um atomo?',
+    'O átomo é uma partícula formada por matéria que não pode ser subdivido.')
+    
+    criaCartao (
+    'Português',
+    'Qual é a figura de linguagem em “O vento cantava uma canção triste”?',
+    'Personificação')
+    
+    criaCartao (
+    'Literatura',
+    'elementos da literatura gótica ?',
+    'ruptuda de padrões literários,afronta racionalismo,instinto vida e morte, libido e terror')
+    
+    criaCartao (
+    'História',
+    'Qual foi o tratado que dividiu o mundo entre Portugal e Espanha em 1494?',
+    'tratado de tordesilhas')
+    
+    criaCartao (
+    'Filosofia',
+    'qual a contribuição de kant?',
+    'reune racionalismo e empiri, revolução copernicana')
+    
+    criaCartao (
+    'Geografia',
+    ' o que é agropecuária extensiva ?',
+    'áreas extensas, baixa modernidade, mão de obra familiar ')
+    
+    criaCartao (
+    'Biologia',
+    'onde ocorre digestão de proteínas e gorduras respectivamente ?',
+    'estômago e intestino ')
+    
+    criaCartao (
+    'Física',
+    'Qual é a velocidade da luz no vácuo?',
+    'Aproximadamente 300.000 km/s')
+    
+    criaCartao (
+    'Matemática',
+    '?Qual a formula da PG',
+    'An = a1 • r^(n-1)')
+    
+    criaCartao (
+    'Química',
+    'Parâmetros para classificação da cadeia carbônica?',
+    'A cadeia carbônica é classificada por ligação, forma, ramificação, composição, arranjo de átomos e número de carbonos, permitindo a identificação de moléculas orgânicas.')
+    
+    criaCartao (
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Camberra')
+    
+    criaCartao (
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    '1789')
+    
+    criaCartao (
+    'Português',
+    'Qual é a função da conjunção “mas” em uma frase?',
+    'Introduzir uma ideia de oposição')
+    
+    criaCartao (
+    'Química',
+    'Qual a fórmula molecular da glicose?',
+    'C6H12O6')
+    
+    criaCartao (
+    'Literatura',
+    'Qual é o movimento literário associado ao poeta Vinicius de Moraes?',
+    'Modernismo')
